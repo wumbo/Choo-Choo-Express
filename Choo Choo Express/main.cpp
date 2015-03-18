905ede0d1024b86e5efff3d76ab001468e62c3f4
Simon Crequer <sjc291@uclive.ac.nz>
2015-03-19 10:36:11 +1300

    Initial Commit

---
+++ Choo Choo Express/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Choo Choo Express
+//
+//  Created by Simon Crequer on 19/03/15.
+//  Copyright (c) 2015 Simon Crequer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
